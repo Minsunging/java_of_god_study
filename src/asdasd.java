e72e37c721609c0a24520e9cebc9a9ad7de73899
민성잉 <gamesung06@naver.com>
2022-08-18 18:23:43 +0900

    Initial commit

---
+++ src/asdasd.java
@@ -0,0 +1,6 @@
+public class asdasd {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+        System.out.println("aa");
+    }
+}
---
+++ src/car.java
@@ -0,0 +1,16 @@
+public class car {
+    int speed;
+    int distance;
+    String color;
+
+    public void Car(){
+
+    }
+
+    public void speedUp(){
+
+    }
+    public void breakDown(){
+
+    }
+}
